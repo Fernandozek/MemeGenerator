239bdab4080363ec272bc8a97d47010b7b1a5a19
Fernando Dutra <fernandozek@hotmail.com>
2021-04-28 02:47:08 -0300

    initial project

---
+++ src/context/HomeContext.tsx
@@ -0,0 +1,114 @@
+import {createContext, MutableRefObject, ReactNode, useEffect, useRef, useState} from 'react';
+
+interface HomeContextData {
+    images: ImageData[];
+    canvasRef: MutableRefObject<HTMLCanvasElement>;
+    upperText: string;
+    lowerText: string;
+    onDrop: (acceptedFiles)=> void;
+    setUpperText: (value:string) => void;
+    setLowerText: (value:string) => void;
+    downloadMeme
+    fontSz
+    fontSize: number;
+}
+
+export const HomeContext = createContext({} as HomeContextData);
+
+interface ImageData {
+    src: string;
+}
+
+interface HomeContextProviderProps {
+    children: ReactNode;
+}
+
+const HomeContextProvider = ({children}:HomeContextProviderProps) => {
+    const [images, setImages] = useState<ImageData[]>([]);
+    const [upperText, setUpperText] = useState("");
+    const [lowerText, setLowerText] = useState("");
+    const canvasRef = useRef(null);
+    const [img, setImg] = useState(0);
+    const [fontSize, setFontSz] = useState(30);
+
+    useEffect(()=> {
+        if (images && images.length) {
+            const canvas:HTMLCanvasElement = canvasRef.current;
+            const contexto = canvas.getContext('2d');
+            var image = new Image();
+            image.onload = ()=> {
+                canvas.width = 500;
+                canvas.height = 500;
+                
+                contexto.drawImage(image, 0, 0, 500, 500);
+                contexto.strokeStyle = "black";
+                contexto.font = `${fontSize}pt Impact`;
+                contexto.lineWidth = 4;
+                contexto.fillStyle = "white";
+                const lines = upperText.split('\n');
+                
+                lines.forEach((line, index)=> {
+                    let deslocamento = 60;
+                    contexto.strokeText(line, 50, deslocamento + index * 40);
+                    contexto.fillText(line, 50, deslocamento + index * 40);
+                });
+                contexto.strokeText(lowerText, 50, 450);
+                contexto.fillText(lowerText, 50, 450);
+            }
+            if(images[img] === undefined){
+                image.src = images[img-1].src;
+            }else{
+                image.src = images[img].src;
+            }
+        }
+    }, [images, upperText, lowerText]);
+
+    const fontSz = (e: number) =>{
+       setFontSz(e); 
+       setUpperText(upperText);
+       return e;
+    }
+    
+    const downloadMeme = () => {
+        if(images && images.length > 0) {
+            const canvas = canvasRef.current;
+            console.log(canvas.toDataURL());
+            const a = document.createElement('a');
+            a.href = canvas.toDataURL();
+            a.download = 'meme.png';
+            document.body.appendChild(a);
+            a.click();
+        }
+    }
+    const onDrop = (filesSelected) => {
+        filesSelected.map(file => {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const updatedImages:ImageData[] = [...images, {src: `${e.target.result}`}];
+            setImages(updatedImages);
+            setImg(img+1);
+        };
+        reader.readAsDataURL(file);
+        return file;
+        });
+    }
+
+    return (
+        <HomeContext.Provider value={{
+            images,
+            upperText,
+            lowerText,
+            onDrop,
+            canvasRef,
+            setUpperText,
+            setLowerText,
+            downloadMeme,
+            fontSz,
+            fontSize
+        }}>
+            {children}
+        </HomeContext.Provider>
+    )
+}
+
+export default HomeContextProvider;
---
+++ src/pages/index.tsx
@@ -0,0 +1,79 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import {useDropzone} from 'react-dropzone';
+import { useContext} from 'react';
+import { HomeContext } from '../context/HomeContext';
+import { Icon } from '@material-ui/core';
+
+export default function Home() {
+  const {
+    canvasRef,
+    upperText,
+    lowerText,
+    setUpperText,
+    setLowerText,
+    onDrop,
+    images,
+    downloadMeme,
+    fontSz,
+    fontSize
+  } = useContext(HomeContext);
+ 
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop, accept: ["image/*"]});
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Fábrica de Memes</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <div className={styles.title}>
+        <h1>Crie memes rápido e de forma fácil</h1>
+      </div>
+      <main className={styles.content}>
+        <div {...getRootProps()} className={styles.canvas}>
+          {
+            isDragActive ? 
+              (
+                <div className={styles.drag}>
+                    <Icon className={styles.uploadicon}>file_upload</Icon>
+                    <h1>Solte aqui</h1>
+                </div>
+              )
+            :
+            (
+                <canvas className={styles.painel} ref={canvasRef}>
+
+                </canvas>
+            )
+          }
+          
+            <input {...getInputProps()} />
+        </div>
+        <form className={styles.form}>
+          <div>
+            <label htmlFor="upperText">Texto superior</label>
+            <textarea 
+              id="upperText"
+              name="upperText"
+              value={upperText}
+              onChange={(e) => setUpperText(e.target.value)}
+            />
+          </div>
+          <div>
+          <label htmlFor="lowerText">Texto inferior</label>
+            <textarea 
+              id="lowerText"
+              name="lowerText"
+              value={lowerText}
+              onChange={(e) => setLowerText(e.target.value)}
+            />
+          </div>
+          <a onClick={() => downloadMeme()}>Save</a>
+          Tamanho da fonte: {fontSize}
+          <input type="range"min="10" max="100" value={fontSize} className={styles.slider} onChange={(e) => fontSz(e.target.value)}/>
+        </form>
+      </main>
+    </div>
+  )
+}
